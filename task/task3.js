03c8703d4d1d0f66bbc11a690963f006e5688d3a
vareshka0101 <vareshka0101@mail.ru>
2025-09-14 16:42:50 +0300

    Доюавлена новая задача на метод reduceна ветку reduce

---
+++ task/task3.js
@@ -0,0 +1,9 @@
+/*Использование reduce:
+ Задача: У вас есть массив чисел. Используйте метод reduce, чтобы найти сумму всех чисел в массиве.*/
+const numbers = [1, 2, 3, 4];
+const sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+
+console.log(sum);
