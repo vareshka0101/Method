80113306322430cdf05a733b75f48779053f5731
vareshka0101 <vareshka0101@mail.ru>
2025-09-14 15:45:14 +0300

    Добавлена задача на метод массива filter

--- task.js
+++ task.js
@@ -5,3 +5,13 @@ const numbers = [1, 2, 3, 4];
 
 const divisNumbers = numbers.map((number) => number * 2);
 console.log(`Новый массив чисел: [${divisNumbers}]`);
+
+/*2. Использование filter:
+Задача: У вас есть массив строк. Используйте метод filter, чтобы отфильтровать строки, длина которых больше 5 символов.*/
+
+
+const strings = ['cat', 'dog', 'bear', 'duck'];
+
+const longStrings = strings.filter(str => str.length > 3);
+console.log(
+  `В данном массиве следующие слова имеют больше 3 символов: ${longStrings.toString()}`);
